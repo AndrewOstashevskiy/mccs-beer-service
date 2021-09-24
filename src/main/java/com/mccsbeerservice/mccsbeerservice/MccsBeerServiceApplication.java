286aded1fff74342fd0355f69759a7111d0d0400
AndrewOstashevskiy <zlsoeoid@gmail.com>
2021-09-24 22:05:17 +0300

    Com 1

---
+++ src/main/java/com/mccsbeerservice/mccsbeerservice/MccsBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mccsbeerservice.mccsbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccsBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MccsBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mccsbeerservice/mccsbeerservice/MccsBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mccsbeerservice.mccsbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccsBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
