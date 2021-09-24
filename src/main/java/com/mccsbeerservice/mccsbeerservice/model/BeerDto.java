f7bd1919e8324625b2e327a8c157f84e940d3293
AndrewOstashevskiy <zlsoeoid@gmail.com>
2021-09-24 22:29:50 +0300

    com 3

---
+++ src/main/java/com/mccsbeerservice/mccsbeerservice/model/BeerDto.java
@@ -0,0 +1,22 @@
+package com.mccsbeerservice.mccsbeerservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+}
---
+++ src/main/java/com/mccsbeerservice/mccsbeerservice/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.mccsbeerservice.mccsbeerservice.model;
+
+public enum BeerStyleEnum {
+    FIRST, SECOND, THIRD
+}
